591f199ac5eb6ac395bac9a1d10a24a5ce354eeb
Harika264 <harikavelugubantla@gmail.com>
2026-03-17 21:43:47 +0530

    Create Combination sum.py

---
+++ Combination sum.py
@@ -0,0 +1,17 @@
+def combination_sum(candidates, target):
+    res = []
+
+    def backtrack(start, total, path):
+        if total == target:
+            res.append(path[:])
+            return
+        if total > target:
+            return
+
+        for i in range(start, len(candidates)):
+            path.append(candidates[i])
+            backtrack(i, total + candidates[i], path)
+            path.pop()
+
+    backtrack(0, 0, [])
+    return res
