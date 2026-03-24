c9b787d96bbef65c294216b34b3b4de9053add65
Harika264 <harikavelugubantla@gmail.com>
2026-03-24 22:03:18 +0530

    Create Non-overlapping Intervals.py

---
+++ Non-overlapping Intervals.py
@@ -0,0 +1,12 @@
+def erase_overlap(intervals):
+    intervals.sort(key=lambda x: x[1])
+    count = 0
+    prev_end = intervals[0][1]
+
+    for start, end in intervals[1:]:
+        if start < prev_end:
+            count += 1
+        else:
+            prev_end = end
+
+    return count
