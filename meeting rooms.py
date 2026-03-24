2d3467508282ee5f75f8cd2ca53b2de048f10e2c
Harika264 <harikavelugubantla@gmail.com>
2026-03-24 22:02:18 +0530

    Create meeting rooms.py

---
+++ meeting rooms.py
@@ -0,0 +1,8 @@
+def can_attend(intervals):
+    intervals.sort()
+
+    for i in range(1, len(intervals)):
+        if intervals[i][0] < intervals[i-1][1]:
+            return False
+
+    return True
